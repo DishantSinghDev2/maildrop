7537800ce8008defa829381acb01feab3ff8be56
Dishant Singh <122022552+DishantSinghDev2@users.noreply.github.com>
2024-09-25 10:12:16 +0000

    Refactor SMTP configuration files to update TLS key and certificate paths

--- smtp/src/plugins/maildrop.deny.js
+++ smtp/src/plugins/maildrop.deny.js
@@ -5,6 +5,13 @@ exports.register = function () {
 
 exports.rcpt = function(next, connection, params) {
   const plugin = this;
+  const recipient = params[0]; // Get the recipient address
+
+  // Allow emails to kodewith.me domain
+  if (recipient && recipient.endsWith('@kodewith.me')) {
+    return next(); // Allow this recipient
+  }
+
   plugin.logwarn(`exports.rcpt ${JSON.stringify(params)}`);
   next(DENY, "Testing");
 };
