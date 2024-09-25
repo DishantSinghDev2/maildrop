42f1649db6dc14130ddc1b3b05962e9e3c7b8b83
Dishant Singh <122022552+DishantSinghDev2@users.noreply.github.com>
2024-09-25 05:29:05 +0000

    Refactor import statements and load dateformat dependency in queue.redis.js

--- smtp/src/plugins/queue.redis.js
+++ smtp/src/plugins/queue.redis.js
@@ -1,5 +1,5 @@
 const shortid = require('shortid');
-const dateformat = await import('dateformat').then(module => module.default);
+const dateformat = import('dateformat');
 const simpleParser = require('mailparser').simpleParser;
 
 exports.register = function() {
