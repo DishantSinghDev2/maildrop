715cb318f08138ba514ef6b34e7c610fcb0d8eda
Dishant Singh <122022552+DishantSinghDev2@users.noreply.github.com>
2024-09-25 13:04:50 +0000

    Refactor rcpt function in maildrop.deny.js to allow emails only to the 'kodewith.me' domain

--- smtp/src/plugins/maildrop.deny.js
+++ smtp/src/plugins/maildrop.deny.js
@@ -15,7 +15,7 @@ exports.rcpt = function(next, connection, params) {
   const recipient_host = params["original_host"]; // Get the recipient address
 
   // Allow emails to kodewith.me domain
-  if (recipient_host && recipient_host.endsWith('@kodewith.me')) {
+  if (recipient_host && recipient_host == 'kodewith.me') {
     return next(); // Allow this recipient
   }
 
